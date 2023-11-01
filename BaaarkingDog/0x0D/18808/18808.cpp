c4bea92de3191cda0a1b52d20ff5aa07a06a9879
yugd1211 <yugd1211@gmail.com>
2023-11-01 18:40:01 +0900

    바킹독 18808 나중에 풀기

---
+++ BaaarkingDog/0x0D/18808/18808.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N, M, K;
+// N = 노트북 세로
+// M = 노트북 가로
+// K = 스티커 개수
+
+int board[42][42];
+vector<pair<int, int> >stickers;
+
+int main()
+{
+    cin >> N;
+    cin >> M;
+    cin >> K;
+    for (int i = 0; i < K; i++)
+    {
+        int R; cin >> R;
+        int C; cin >> C;
+        for (int j = 0; j < R; j++)
+        {
+            for (int k = 0; k < C; k++)
+            {
+                
+            }
+        }
+    }
+    return 0;
+}
