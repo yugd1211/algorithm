b3adab5a7863c5bf4028d629f67943ee13352e9f
yugd1211 <yugd1211@gmail.com>
2023-12-11 15:58:37 +0900

    [백준 11055: 가잔 큰 증가하는 부분 수열] LIS 알고리즘을 활용해서 푸는 문제인데 처음 문제를 맞이했을때 LIS를 모르기 때문에 비슷하게 풀려고 노력했지만 어려웠다. LIS를 배우는데 조금 시간이 걸렸지만 어떻게 푸는 지 알기만하면 오래 걸리는 문제는 아니였다

--- backjoon/11055.cpp
+++ backjoon/11055.cpp
@@ -3,20 +3,31 @@
 
 using namespace std;
 
+// LIS 알고리즘 문제로
+// dp에 자신보다 작은 수들 + 자신을 더한값을 업데이트하는 방식
+
 int N;
 int A[1001];
+int dp[1001];
 int main()
 {
     cin >> N;
     for (int i = 0; i < N; i++)
+    {
         cin >> A[i];
+        dp[i] = A[i];
+    }
     int ans = 0;
     int prev = 10000;
-    int curr;
-    for (int i = N - 1; i >= 0; i--)
+    for (int curr = 0; curr < N; curr++)
     {
-        curr = A[i];
-        if (curr < prev)
-            ans += curr;
+        for (int j = 0; j < curr; j++)
+        {
+            if (A[j] < A[curr])
+                dp[curr] = max(dp[j] + A[curr], dp[curr]);
+        }
     }
+    for (int i = 0; i < N; i++)
+        ans = max(ans, dp[i]);
+    cout << ans;
 }
