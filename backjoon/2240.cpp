7a0f2af9571bdc9821cef67c9533c723554e6f7c
yugd1211 <yugd1211@gmail.com>
2024-01-29 14:14:28 +0900

    [백준 2240 : 자두나무] DP 문제로 DP 문제에 익숙하지 않았을때 문제를 접했을때는 어떻게 풀어야할지도 감이 안잡혔다. 시간을 두고 다시 문제를 보니 어느정도 풀이는 떠올랐지만 막상 코드로 옮기려고하니까 잘 작성되지 않아서 예시에 나오는 입출력을 손으로 그리면서 내가 생각한 풀이를 그대로 옮겨보면서 표를 작성하니 단계별로 어떤 연산이 필요할지가 정리되었다.

--- backjoon/2240.cpp
+++ backjoon/2240.cpp
@@ -4,76 +4,62 @@
 #include <queue>
 using namespace std;
 
+// 문제 풀이
+// DP로 접근해서 문제를 풀었다.
+// 자두가 떨어지는 시간마다 모든 횟수에 대해 연산을 한다.
+// 0부터 W까지 DP 배열을 업데이트하는데 그 방법은 다음과 같다.
+// 일단 현재 횟수의 이전 시간까지 먹을 수 있는 자두 갯수 + 현재시간에 현재위치에 떨어지는 자두를 업데이트한다.
+// 그러면 이전횟수의 값이 그대로 담기거나, +1이 담기게 될것이다.
+// 그리고 '이전횟수의 반대되는 위치의 값 + 현재위치에 자두가 떨어지면 그것도 포함'이랑 현재위치의 값을 비교해서 업데이트한다.
+
 int T, W;
-int board[1001][2];
-int vis[1001][2][31];
+int board[2][1001];
+int vis[2][1001][31];
 
 void printBoard()
 {
-    for (int i = 0; i < T; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << board[i][j] << " ";
-        }
-        cout << "\n";
-    }
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    // 현재 깊이, 어떤 나무밑에 있는지
-    queue<tuple<int, bool, int>> q;
-    cin >> T >> W;
-    for (int i = 1; i <= T; i++)
-    {
-        int n;
-        cin >> n;
-        board[i][n - 1] = 1;
-    }
-    q.push({0, 0, 0});
-    while (!q.empty())
-    {
-        int h, mc, w;
-        tie(h, w, mc) = q.front();
-        q.pop();
-        if (h == T)
-            continue;
-        if (vis[h + 1][w][mc] < vis[h][w][mc] + 1 || vis[h + 1][w][mc] < vis[h][w][mc])
-        {
-            q.push({h + 1, w, mc});
-            vis[h + 1][w][mc] = vis[h][w][mc];
-            if (board[h + 1][w])
-                vis[h + 1][w][mc]++;
-        }
-        if (mc >= W)
-            continue;
-        if (board[h + 1][w])
-            continue;
-        if (vis[h + 1][(w + 1) % 2][mc + 1] < vis[h][w][mc] + 1)
-        {
-            q.push({h + 1, (w + 1) % 2, mc + 1});
-            vis[h + 1][(w + 1) % 2][mc + 1] = vis[h][w][mc] + 1;
-        }
-    }
-    cout << "===================================================\n";
-    int ans = 0;
+    cout << "=======print board========\n";
     for (int i = 1; i <= T; i++)
     {
         for (int j = 0; j <= W; j++)
         {
             for (int k = 0; k < 2; k++)
             {
-                // if (i == 1 && j == 1 && k == 1)
-                // cout << "vis = " << vis[i][k][j];
-                cout << vis[i][k][j] << " ";
-                ans = max(ans, vis[i][k][j]);
+                cout << vis[k][i][j] << " ";
             }
             cout << " ";
         }
         cout << "\n";
     }
-    cout << ans;
+    cout << "==========================\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T >> W;
+    for (int i = 1; i <= T; i++)
+    {
+        int n;
+        cin >> n;
+        board[n - 1][i] = 1;
+    }
+    for (int i = 1; i <= T; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                if (j == 0 && k == 1)
+                    continue;
+                vis[k][i][j] = vis[k][i - 1][j] + board[k][i];
+                if (j == 0)
+                    continue;
+                vis[k][i][j] = max(vis[k][i][j], vis[(k + 1) % 2][i - 1][j - 1] + board[k][i]);
+            }
+        }
+    }
+    // printBoard();
+    cout << max(vis[0][T][W], vis[1][T][W]);
 }
