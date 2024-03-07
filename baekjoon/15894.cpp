37a755b1886e587d0d7ca3b27fcbbdd9e74b4802
yurudehd <gyyu@student.42seoul.kr>
2024-03-07 23:05:40 +0900

    [백준 15894 : 수학은 체육과목 입니다]

---
+++ baekjoon/15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	long long N;
+	cin >> N;
+	cout << N * 4;
+}
