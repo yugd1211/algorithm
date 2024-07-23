c1138fe05be6d102496456fb61c5294f635a509f
yurudehd <gyyu@student.42seoul.kr>
2024-07-23 16:31:30 +0900

    [백준 14503 : 로봇 청소기]

--- baekjoon/14503.cpp
+++ baekjoon/14503.cpp
@@ -3,13 +3,25 @@
 #include <vector>
 using namespace std;
 
-int N, M;
-int ans = 0;
-int board[51][51];
-int vis[51][51];
+#define NON_CLEAR 0
+#define WALL 1
+#define CLEAR 2
 
+int N, M;
+int r, c, d;
+int ans;
+int cnt = 2;
+int board[51][51];
 int dirX[4] = {-1, 0, 1, 0};
 int dirY[4] = {0, 1, 0, -1};
+
+void input()
+{
+	cin >> N >> M >> r >> c >> d;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> board[i][j];
+}
 
 void print_board()
 {
@@ -17,60 +29,83 @@ void print_board()
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
-		{
 			cout << board[i][j] << " ";
-		}
 		cout << "\n";
 	}
 }
 
-// 0 = 북, 1 = 동, 2 = 남, 3 = 서
-void move_robot(int x, int y, int d)
+bool is_clearable(int x, int y)
 {
-	if (board[x][y] == 0 && !vis[x][y])
-	{
-		vis[x][y] = 1;
-		board[x][y] = 2;
-		print_board();
-
-		ans++;
-	}
 	for (int i = 0; i < 4; i++)
 	{
 		int nx = x + dirX[i];
 		int ny = y + dirY[i];
 		if (nx < 0 || ny < 0 || nx >= N || ny >= M)
 			continue;
-		if (board[nx][ny] != 0 || vis[nx][ny])
+		if (board[nx][ny] != NON_CLEAR)
 			continue;
-		move_robot(nx, ny, d);
-		return;
-		--d == -1 ? 3 : d;
+		return true;
 	}
-	int nx = x + dirX[(d + 2) % 4];
-	int ny = y + dirY[(d + 2) % 4];
-	if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-		return;
-	if (board[nx][ny] == 1)
-		return;
-	move_robot(nx, ny, d);
+	return false;
+}
+
+bool is_clear_curr(int x, int y)
+{
+	if (board[x][y] == NON_CLEAR)
+		return true;
+	else
+		return false;
+}
+
+// 0 = 북, 1 = 동, 2 = 남, 3 = 서
+bool move(int &x, int &y, int &d)
+{
+	if (is_clear_curr(x, y))
+	{
+		board[x][y] = cnt++;
+		ans++;
+	}
+	if (is_clearable(x, y))
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			d--;
+			if (d == -1)
+				d = 3;
+			int nx = x + dirX[d];
+			int ny = y + dirY[d];
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+				continue;
+			if (board[nx][ny] != NON_CLEAR)
+				continue;
+			x = nx;
+			y = ny;
+			break;
+		}
+		return true;
+	}
+	else
+	{
+		int back_dir = (d + 2) % 4;
+		int nx = x + dirX[back_dir];
+		int ny = y + dirY[back_dir];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+			return false;
+		if (board[nx][ny] == WALL)
+			return false;
+		x = nx;
+		y = ny;
+		return true;
+	}
 }
 
 int main()
 {
-
 	cin.tie(0);
 	ios::sync_with_stdio(0);
-	int r, c, d;
-	cin >> N >> M >> r >> c >> d;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> board[i][j];
-		}
-	}
-	move_robot(r, c, d);
-	print_board();
+
+	input();
+	while (move(r, c, d))
+		;
 	cout << ans;
 }
