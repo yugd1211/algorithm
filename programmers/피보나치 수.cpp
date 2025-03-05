a82d5c03bc251cbcb33cc441a7ac975bb715f3d4
yugd1211 <yugd1211@gmail.com>
2025-03-05 21:01:43 +0900

    [프로그래머스] 피보나치 수

---
+++ programmers/피보나치 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long dp[100001];
+
+int solution(int n)
+{
+	dp[0] = 0;
+	dp[1] = 1;
+	for (int i = 2; i < 100001; i++)
+		dp[i] = dp[i - 1] + dp[i - 2] % (long long)1234567;
+	return dp[n] % 1234567;
+}
